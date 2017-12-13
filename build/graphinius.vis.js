752d9268f8076c9c9d508aaf9fc56a331b7f2422
Bernd Malle <bernd.malle@gmail.com>
2017-12-13 23:58:46 +0100

    outsourced hammer touch controls into own file, freestyle not working

--- build/graphinius.vis.js
+++ build/graphinius.vis.js
@@ -57,8 +57,8 @@
 	    fullscreen      = __webpack_require__(5),
 	    interaction     = __webpack_require__(18),
 	    navigation      = __webpack_require__(19),
-	    controlUI       = __webpack_require__(4);
-
+	    navigation      = __webpack_require__(20),    controlUI       = __webpack_require__(4);
+	    
 
 	var out = typeof window !== 'undefined' ? window : global;
 
@@ -2413,91 +2413,6 @@
 	}
 
 
-	/**
-	 * Hammer.js Controls (Touch)
-	 */ 
-
-	var main_el = document.getElementById("main_vis");
-
-	var mc = new Hammer.Manager(main_el, {
-		recognizers: [
-	    // RecognizerClass, [options], [recognizeWith, ...], [requireFailure, ...]
-	    [Hammer.Pan],
-	    [Hammer.Rotate],
-	    [Hammer.Press],
-			[Hammer.Pinch, { enable: true }, ['rotate']],
-			[Hammer.Swipe], // ,{ direction: Hammer.DIRECTION_HORIZONTAL }
-		]
-	});
-
-	var old_rotation = 0;
-	mc.on("rotate", function(ev) {
-	  if ( !ev.srcEvent.shiftKey ) {
-	    return;
-	  }
-	  // console.log(ev);  
-	  if ( ev.rotation < old_rotation ) {
-	    network.rotateOnAxis(axis_z, defaults.delta_rotation/2);
-	    axis_x.applyAxisAngle(axis_z, -defaults.delta_rotation/2);
-	    axis_y.applyAxisAngle(axis_z, -defaults.delta_rotation/2);
-	  }
-	  else {
-	    network.rotateOnAxis(axis_z, -defaults.delta_rotation/2);
-	    axis_x.applyAxisAngle(axis_z, defaults.delta_rotation/2);
-	    axis_y.applyAxisAngle(axis_z, defaults.delta_rotation/2);
-	  }
-	  old_rotation = ev.rotation;
-	})
-
-	var old_scale = 1;
-	mc.on("pinch", function(ev) {
-	  // Not sure if this can be the case...
-	  if ( ev.pointerType === 'mouse' ) {
-	    return;
-	  }
-	  var new_z_pos = globals.camera.position.z - (ev.scale-old_scale) * 20;
-	  new_z_pos = Math.min(new_z_pos, defaults.MAX_CAM_DISTANCE);
-	  new_z_pos = Math.max(new_z_pos, defaults.MIN_CAM_DISTANCE);
-	  globals.camera.position.z = new_z_pos;
-	  window.requestAnimationFrame(update);
-	});
-
-
-	var old_pos_x = globals.camera.position.x;
-	var old_pos_y = globals.camera.position.y;
-	var old_deltaX = 0;
-	mc.on("pan", function(ev) {
-	  // console.log(ev);
-	  if ( ev.pointerType === 'mouse' ) {
-	    return;
-	  }
-	  if ( ev.srcEvent.shiftKey ) {
-	    freeStyle( ev.srcEvent );
-	  }
-	  else if ( ev.srcEvent.altKey ) {
-	    if ( ev.deltaX > old_deltaX ) {
-	      network.rotateOnAxis(axis_y, defaults.delta_rotation);
-	      axis_x.applyAxisAngle(axis_y, -defaults.delta_rotation);
-	    }
-	    else {
-	      network.rotateOnAxis(axis_y, -defaults.delta_rotation);
-	      axis_x.applyAxisAngle(axis_y, defaults.delta_rotation);
-	    }
-	    old_deltaX = ev.deltaX;
-	  }
-	  else {
-	    globals.camera.position.x = old_pos_x - ev.deltaX * globals.camera.position.z / 500;
-	    globals.camera.position.y = old_pos_y + ev.deltaY * globals.camera.position.z / 500;
-	    confineXYMovement();
-	  }
-	  window.requestAnimationFrame(update);
-	});
-
-	mc.on("panend", function(ev) {
-	  old_pos_x = globals.camera.position.x;
-	  old_pos_y = globals.camera.position.y;
-	});
-
 
 	container.element.addEventListener('mousemove', mouseMove, false);
 	function mouseMove(event) {
@@ -2592,8 +2507,117 @@
 	// }
 
 	module.exports = {
-	  mouse: mouse
+	  mouse: mouse,
+	  freeStyle: freeStyle
 	};
+
+
+/***/ }),
+/* 20 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	var keys = __webpack_require__(1).keys;
+	var globals = __webpack_require__(1).globals;
+	var defaults = __webpack_require__(1).defaults;
+	var update = __webpack_require__(2).update;
+	var network = __webpack_require__(1).globals.network;
+	var container = __webpack_require__(1).container;
+	var mouse = __webpack_require__(1).globals.mouse;
+	var nodeIntersection = __webpack_require__(18).nodeIntersection;
+	var callbacks = __webpack_require__(1).callbacks;
+	var freeStyle = __webpack_require__(19).freeStyle;
+
+	/**
+	 * Hammer.js Controls (Touch)
+	 */ 
+
+
+	 //rotation
+	var axis_x = new THREE.Vector3( 1, 0, 0 ),
+	axis_y = new THREE.Vector3( 0, 1, 0 ),
+	axis_z = new THREE.Vector3( 0, 0, 1 );
+
+
+	var main_el = document.getElementById("main_vis");
+
+	var mc = new Hammer.Manager(main_el, {
+		recognizers: [
+	    // RecognizerClass, [options], [recognizeWith, ...], [requireFailure, ...]
+	    [Hammer.Pan],
+	    [Hammer.Rotate],
+	    [Hammer.Press],
+			[Hammer.Pinch, { enable: true }, ['rotate']],
+			[Hammer.Swipe], // ,{ direction: Hammer.DIRECTION_HORIZONTAL }
+		]
+	});
+
+	var old_rotation = 0;
+	mc.on("rotate", function(ev) {
+	  if ( !ev.srcEvent.shiftKey ) {
+	    return;
+	  }
+	  // console.log(ev);  
+	  if ( ev.rotation < old_rotation ) {
+	    network.rotateOnAxis(axis_z, defaults.delta_rotation/2);
+	    axis_x.applyAxisAngle(axis_z, -defaults.delta_rotation/2);
+	    axis_y.applyAxisAngle(axis_z, -defaults.delta_rotation/2);
+	  }
+	  else {
+	    network.rotateOnAxis(axis_z, -defaults.delta_rotation/2);
+	    axis_x.applyAxisAngle(axis_z, defaults.delta_rotation/2);
+	    axis_y.applyAxisAngle(axis_z, defaults.delta_rotation/2);
+	  }
+	  old_rotation = ev.rotation;
+	})
+
+	var old_scale = 1;
+	mc.on("pinch", function(ev) {
+	  // Not sure if this can be the case...
+	  if ( ev.pointerType === 'mouse' ) {
+	    return;
+	  }
+	  var new_z_pos = globals.camera.position.z - (ev.scale-old_scale) * 20;
+	  new_z_pos = Math.min(new_z_pos, defaults.MAX_CAM_DISTANCE);
+	  new_z_pos = Math.max(new_z_pos, defaults.MIN_CAM_DISTANCE);
+	  globals.camera.position.z = new_z_pos;
+	  window.requestAnimationFrame(update);
+	});
+
+
+	var old_pos_x = globals.camera.position.x;
+	var old_pos_y = globals.camera.position.y;
+	var old_deltaX = 0;
+	mc.on("pan", function(ev) {
+	  // console.log(ev);
+	  if ( ev.pointerType === 'mouse' ) {
+	    return;
+	  }
+	  if ( ev.srcEvent.shiftKey ) {
+	    freeStyle( ev.srcEvent );
+	  }
+	  else if ( ev.srcEvent.altKey ) {
+	    if ( ev.deltaX > old_deltaX ) {
+	      network.rotateOnAxis(axis_y, defaults.delta_rotation);
+	      axis_x.applyAxisAngle(axis_y, -defaults.delta_rotation);
+	    }
+	    else {
+	      network.rotateOnAxis(axis_y, -defaults.delta_rotation);
+	      axis_x.applyAxisAngle(axis_y, defaults.delta_rotation);
+	    }
+	    old_deltaX = ev.deltaX;
+	  }
+	  else {
+	    globals.camera.position.x = old_pos_x - ev.deltaX * globals.camera.position.z / 500;
+	    globals.camera.position.y = old_pos_y + ev.deltaY * globals.camera.position.z / 500;
+	    confineXYMovement();
+	  }
+	  window.requestAnimationFrame(update);
+	});
+
+	mc.on("panend", function(ev) {
+	  old_pos_x = globals.camera.position.x;
+	  old_pos_y = globals.camera.position.y;
+	});
 
 
 /***/ })
--- src/view/navigation.js
+++ src/view/navigation.js
@@ -104,91 +104,6 @@ function mousewheel(event) {
 }
 
 
-/**
- * Hammer.js Controls (Touch)
- */ 
-
-var main_el = document.getElementById("main_vis");
-
-var mc = new Hammer.Manager(main_el, {
-	recognizers: [
-    // RecognizerClass, [options], [recognizeWith, ...], [requireFailure, ...]
-    [Hammer.Pan],
-    [Hammer.Rotate],
-    [Hammer.Press],
-		[Hammer.Pinch, { enable: true }, ['rotate']],
-		[Hammer.Swipe], // ,{ direction: Hammer.DIRECTION_HORIZONTAL }
-	]
-});
-
-var old_rotation = 0;
-mc.on("rotate", function(ev) {
-  if ( !ev.srcEvent.shiftKey ) {
-    return;
-  }
-  // console.log(ev);  
-  if ( ev.rotation < old_rotation ) {
-    network.rotateOnAxis(axis_z, defaults.delta_rotation/2);
-    axis_x.applyAxisAngle(axis_z, -defaults.delta_rotation/2);
-    axis_y.applyAxisAngle(axis_z, -defaults.delta_rotation/2);
-  }
-  else {
-    network.rotateOnAxis(axis_z, -defaults.delta_rotation/2);
-    axis_x.applyAxisAngle(axis_z, defaults.delta_rotation/2);
-    axis_y.applyAxisAngle(axis_z, defaults.delta_rotation/2);
-  }
-  old_rotation = ev.rotation;
-})
-
-var old_scale = 1;
-mc.on("pinch", function(ev) {
-  // Not sure if this can be the case...
-  if ( ev.pointerType === 'mouse' ) {
-    return;
-  }
-  var new_z_pos = globals.camera.position.z - (ev.scale-old_scale) * 20;
-  new_z_pos = Math.min(new_z_pos, defaults.MAX_CAM_DISTANCE);
-  new_z_pos = Math.max(new_z_pos, defaults.MIN_CAM_DISTANCE);
-  globals.camera.position.z = new_z_pos;
-  window.requestAnimationFrame(update);
-});
-
-
-var old_pos_x = globals.camera.position.x;
-var old_pos_y = globals.camera.position.y;
-var old_deltaX = 0;
-mc.on("pan", function(ev) {
-  // console.log(ev);
-  if ( ev.pointerType === 'mouse' ) {
-    return;
-  }
-  if ( ev.srcEvent.shiftKey ) {
-    freeStyle( ev.srcEvent );
-  }
-  else if ( ev.srcEvent.altKey ) {
-    if ( ev.deltaX > old_deltaX ) {
-      network.rotateOnAxis(axis_y, defaults.delta_rotation);
-      axis_x.applyAxisAngle(axis_y, -defaults.delta_rotation);
-    }
-    else {
-      network.rotateOnAxis(axis_y, -defaults.delta_rotation);
-      axis_x.applyAxisAngle(axis_y, defaults.delta_rotation);
-    }
-    old_deltaX = ev.deltaX;
-  }
-  else {
-    globals.camera.position.x = old_pos_x - ev.deltaX * globals.camera.position.z / 500;
-    globals.camera.position.y = old_pos_y + ev.deltaY * globals.camera.position.z / 500;
-    confineXYMovement();
-  }
-  window.requestAnimationFrame(update);
-});
-
-mc.on("panend", function(ev) {
-  old_pos_x = globals.camera.position.x;
-  old_pos_y = globals.camera.position.y;
-});
-
 
 container.element.addEventListener('mousemove', mouseMove, false);
 function mouseMove(event) {
@@ -283,5 +198,6 @@ function confineXYMovement() {
 // }
 
 module.exports = {
-  mouse: mouse
+  mouse: mouse,
+  freeStyle: freeStyle
 };
